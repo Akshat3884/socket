0f082812c4c1ae861bb61d6db1c027e3a2c6bfda
Akshat3884 <59174408+Akshat3884@users.noreply.github.com>
2021-03-17 19:06:21 +0530

    Update client.py

--- client.py
+++ client.py
@@ -1,4 +1,8 @@
-# Import socket module  
+####################################################################
+# Import socket module and 
+# Threading for running multiple pieces of code at the same time
+####################################################################
+
 import socket    
 import threading          
   
@@ -9,17 +13,18 @@ s = socket.socket()
 port = 12345                
   
 # connect to the server on local computer  
-s.connect(('localhost', port))  
+s.connect(('192.168.25.60', port))  
 
 # Taking the username of the user
 # and sending it to the server
 name = input("Enter a username: ")
 s.send(bytes(name,'utf-8'))
 
-# for recieving thanks for connecting at the begining
+# For Recieving thanks for connecting at the begining
 print(s.recv(1024).decode())
 
-# Defining func for handeling incoming functions 
+# Defining func for handeling incoming messages 
+# From the server
 def incoming_messages():
     incoming_message = s.recv(1024).decode()
     
@@ -29,15 +34,18 @@ def incoming_messages():
         # Printing the recieved input on another signal 
         print(incoming_message+"\n")
 
-
+#####################################################################
 # main loop to run forever
 # Taking input and giving it to the server
 while True:
     try:
+        # Taking the Main input from the client 
         message = input("> ")
-
+        # Tweaking the input and sending it to the server
         new_message = f"{name} SENT : " + message
+
         sent_message = s.send(new_message.encode())
+
         recieving_thread = threading.Thread(target=incoming_messages)
         recieving_thread.start()
             
@@ -46,5 +54,3 @@ while True:
         print("Please check if the server is running ")
         s.close()
         break
-
-   
